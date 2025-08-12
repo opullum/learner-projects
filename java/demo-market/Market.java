70adf4f2d963f9092cd9cf9d8ebb94fe89d3b95e
opullum <octavian.pullum@gmail.com>
2025-08-13 01:16:37 +0900

    Comments added to source. Minor output formatting changes. Marking minimum completion of learner project.

--- java/demo-market/Market.java
+++ java/demo-market/Market.java
@@ -14,7 +14,7 @@ import java.util.Scanner;
 public class Market {
 
     public static void printMenu() {
-        System.out.println("SHOPPING CART MENU");
+        System.out.println("\nSHOPPING CART MENU");
         System.out.println("1. Add item to the cart");
         System.out.println("2. View items in cart");
         System.out.println("3. Checkout items");
@@ -22,9 +22,10 @@ public class Market {
     }
 
     public static void main(String[] args) {
-        Cart shoppingCart = new Cart();             // Shopping cart to store items
-        Scanner input = new Scanner(System.in);     // Retrieving user input
+        Cart shoppingCart = new Cart();                             // Shopping cart to store items
+        Scanner input = new Scanner(System.in);                     // Retrieving user input
 
+        // Default value of -1 placed here. Will not trigger any options, leading to user input prompt
         int selection = -1;
 
         do {
@@ -32,13 +33,21 @@ public class Market {
             System.out.print("\nEnter your selection: ");
 
             try {
+                // .nextFoo() will not stop at the ENTER key. Therefore, we convert to relevant data type
+                // to clear the scanner
                 String line = input.nextLine();
                 selection = Integer.parseInt(line);
+
+                // Checking for invalid inputs
                 if (selection < 0 || selection > 3) {
-                    System.out.println("Invalid Selection. Please select a choice from the given options.");
+                    System.out.println("Invalid Selection. Please select a choice from the given options.\n");
                 } else {
+
+                    // INPUT HANDLER (1: Add Item, 2: View Items, 3: Checkout Items, 0: Exit)
                     switch (selection) {
                         case 1:
+                            // CASE ONE: CREATING NEW ITEM
+                            // Creates a new instance of the Item class to be placed in shoppingCart
                             String itemName;
                             double itemPrice;
                             System.out.print("Enter a name for the item: ");
@@ -53,29 +62,35 @@ public class Market {
                                 System.out.println("Please enter a numerical value for the price");
                                 System.out.println("Cancelling item creation...");
                             }
-
                             break;
+
                         case 2:
+                            // CASE TWO: LIST SHOPPING CART ITEMS
+                            // Displays all Item instances in shoppingCart, which returns an Item[] when
+                            // Cart.returnCart() is called
                             Item[] items = shoppingCart.returnCart();
                             if (items.length == 0) {
                                 System.out.println("Shopping Cart is Empty!");
                             } else {
+                                System.out.println("DISPLAYING SHOPPING CART ITEMS...");
                                 for (Item item: shoppingCart.returnCart()) {
                                     System.out.println(item);
                                 }
                             }
                             break;
                         case 3:
+                            // CASE 3: ITEM CHECKOUT
+                            // Displays the total price for all the items. Clears cart afterward.
                             System.out.println("CHECKING OUT ITEMS...");
                             double totalPrice = 0;
                             for (Item item : shoppingCart.returnCart()) {
                                 System.out.println(item);
                                 totalPrice += item.getPrice();
                             }
-                            System.out.printf("\n\nFINAL PRICE: %f", totalPrice);
+                            System.out.printf("\nFINAL PRICE: %f\n", totalPrice);
                             shoppingCart = new Cart();
-
                             break;
+
                         case 0:
                             System.out.println("STOPPING PROGRAM");
                             break;
