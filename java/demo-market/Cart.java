8a6cdb390d784cfcb4dc5c5f31645760d217982c
opullum <octavian.pullum@gmail.com>
2025-08-12 23:42:32 +0900

    Initial commit.

---
+++ java/demo-market/Cart.java
@@ -0,0 +1,14 @@
+/**
+ * Cart (Class)
+ * Meant to hold basic items. Items will be sorted into an array containing Items
+ * @author Octavian Pullum
+ * @version 1.0
+ * @since 1.0
+ */
+
+package shopping;
+
+public class Cart {
+
+
+}
---
+++ java/demo-market/Market.java
@@ -0,0 +1,18 @@
+/**
+ * Market Handler
+ * Basic text-based simulator of a shopping cart. Program to practice
+ * creating basic input/output using Scanner objects.
+ * @author Octavian Pullum
+ * @version 1.0
+ * @since 1.0
+ */
+
+import shopping.Cart;
+
+public class Market {
+
+
+
+
+
+}
