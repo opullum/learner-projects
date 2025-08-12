af2f29b81f3118076c833c260b7b35a4796f0005
opullum <octavian.pullum@gmail.com>
2025-08-12 23:46:47 +0900

    Initial commit.

---
+++ java/demo-market/Item.java
@@ -0,0 +1,16 @@
+/**
+ * Item (Class)
+ * Represents individual items. Meant to be used with class Cart.
+ * Items contain a price and name. Retrievable through getter mthods.
+ * @author Octavian Pullum
+ * @version 1.0
+ * @since 1.0
+ */
+
+package shopping;
+
+public class Item {
+
+
+
+}
