63c5b3cc54d2a36b016888a9bbe4ddf343393816
opullum <octavian.pullum@gmail.com>
2025-08-29 20:18:41 +0900

    Completed program Confirmed functionality.

--- python/geeks-for-geeks/number-guessing/main.py
+++ python/geeks-for-geeks/number-guessing/main.py
@@ -12,4 +12,18 @@ total_guesses = 0
 generated_number = random.randint(0, 100)
 
 while (guessed_number != generated_number):
-    print(f"\nEnter a number to be guessed: ")
+    
+    try:
+        guessed_number = int(input(f"\nEnter a number to be guessed: "))
+    except ValueError:
+        print(" !! [INVALID GUESS TYPE] !! ")
+
+    if (type(guessed_number) != int) or (guessed_number < 0) or (guessed_number > 100):
+        print("Please only enter positive integer values (0 - 100) for guesses.")
+    else:
+        total_guesses += 1
+        if guessed_number < generated_number: print("Too Low!\n")
+        elif guessed_number > generated_number: print("Too High!\n")
+
+print(f"Congratulations on guessing the number {generated_number}!\n")
+print(f"You took {total_guesses} guesses to find the correct number!")
