bc3209b0b96a077faf1297083ed57f2e67bf593e
opullum <octavian.pullum@gmail.com>
2025-08-29 21:41:11 +0900

    Confirmed basic program functionality. Marking completion of project.

--- python/geeks-for-geeks/word-guessing/main.py
+++ python/geeks-for-geeks/word-guessing/main.py
@@ -1,17 +1,66 @@
 # Word Guessing Game
 # User attempts to guess a word hangman style from a set of predefined options.
 # Limited number of guesses before game fails
-    # Can be modified by changing the variable
+# Can be modified by changing the variable
 # Retrieve the name of the user and refer to the name throughout the program
 
 import random
 
 GUESS_COUNT = 12
 
+
+# Displays the word with the correctly guessed characters being visible and the
+# non-guessed characters being represented with a "-"
+def display_word(word: str, guessed_chars: list) -> None:
+    output_string = ""
+    for char in word:
+        if char in guessed_chars:
+            output_string += char
+        else:
+            output_string += "-"
+    print(output_string)
+
+
+guesses = 0
+word_guessed = False
+word_list = []
+guess_list = []
+
+available_words = ["TESTING", "COMPUTER", "SCIENCE"]
+
 name = input("Please enter your name for the Guessing Game: ")
 print(f"Welcome to the game, {name}")
 
-guesses = 0
+
+selected_word = random.choice(available_words)
+
+for char in selected_word:
+    if char not in word_list:
+        word_list.append(char)
+
+while guesses < GUESS_COUNT and not word_guessed:
+    guessed_char = input(f"Enter a character to guess: ")
+    guessed_char = guessed_char[0]
+    guessed_char = str.upper(guessed_char)
+
+    if guessed_char not in guess_list:
+        guess_list.append(guessed_char)
+        guesses += 1
+    else:
+        print(
+            f"You have already guessed the character {guessed_char}! Please try again.\n"
+        )
+
+    display_word(selected_word, guess_list)
+
+    if set(word_list).issubset(guess_list):
+        word_guessed = True
+
+if word_guessed and (guesses < GUESS_COUNT):
+    print(f"Congratulations, {name}! You win. The word was {selected_word}\n")
+    print(f"You took {guesses} tries to guess the word correctly!")
+else:
+    print(f"Oh no! You lost. The correct word was {selected_word}")
 
 # planned algorithm
 #
@@ -37,4 +86,3 @@ guesses = 0
 #       you win!!
 #   else:
 #       you lose!!
-
