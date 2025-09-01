167b1cbae7b81bd33638bde7c30b38fbe478d915
opullum <octavian.pullum@gmail.com>
2025-09-02 01:42:54 +0900

    Created function computer_turn() in Bagram Game (bagram-game/main.py).

--- python/geeks-for-geeks/bagram-game/main.py
+++ python/geeks-for-geeks/bagram-game/main.py
@@ -41,6 +41,56 @@ Else:
 
 import sys
 
+def computer_lost() -> bool:
+
+    continue_playing = True
+
+    print("OH NO! IT APPEARS THE COMPUTER HAS LOST!")
+    print("CONGRATULATIONS ON WINNING!")
+    print("WOULD YOU LIKE TO KEEP PLAYING?")
+    print("ENTER 'YES' or 'NO' AS AN ANSWER!")
+    response = input("> ").lower()
+
+    if (response == 'no'): continue_playing = False
+
+    return continue_playing
+
+def player_lost() -> bool:
+    return True
+
+def computer_turn(called_numbers: list) -> bool:
+
+    recent_number = 0
+    target_distance = 0
+    game_ended = False
+
+    # Calculate target value for the computer to attempt to call towards
+    if called_numbers:
+        recent_number = called_numbers[-1]
+        target_distance = 4 - (recent_number % 4) 
+
+        if recent_number == 20: 
+            computer_lost()
+            game_ended = True
+
+    if (not game_ended):
+        if target_distance > 3: target_distance = 3
+
+        added_num = recent_number + 1
+
+        while added_num < recent_number + target_distance:        
+            called_numbers.append(added_num)
+            added_num += 1   
+
+        recent_number = called_numbers[-1]
+
+        if recent_number == 20: 
+            continue_playing = player_lost()
+            game_ended = True
+
+    return game_ended
+
+
 called_numbers = []
 recent_number = None
 playing = True
@@ -58,6 +108,8 @@ if not (response == "first" or response == "second"):
 
 if (response == "second"):
     print("THEN YOU WILL GO SECOND. ALLOW US TO BEGIN.")
+
+    
 
 
 
