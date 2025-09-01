dd67b28c85ced9e2680ebcbd7c23f4f54afc8a5c
opullum <octavian.pullum@gmail.com>
2025-09-02 00:04:11 +0900

    Initial commit. bagram-game.py

---
+++ python/geeks-for-geeks/bagram-game/main.py
@@ -0,0 +1,6 @@
+# Bagram Game Simulation
+#
+# Simulating the game 'Bagram', a game where the player avoids being the one
+# to say the number 21. Players may choose to call up to 3 consecutive numbers.
+#
+# Calling non-consecutive numbers is an immediate loss. 
