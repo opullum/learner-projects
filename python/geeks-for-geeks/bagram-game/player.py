d972a65db4af3deefb02c365f97a7ade95fde8b3
opullum <octavian.pullum@gmail.com>
2025-09-02 23:31:23 +0900

    Implemented function(s) player_turn and created new function verify_input to verify entered numbers in Bagram Game.

--- python/geeks-for-geeks/bagram-game/player.py
+++ python/geeks-for-geeks/bagram-game/player.py
@@ -1,3 +1,15 @@
+def verify_input(called_nums: list) -> bool:
+
+    if not called_nums: print("HOW DID YOU GET HERE??")
+
+    latest_num = called_nums[-1]
+
+    if latest_num < 0 or latest_num >= 21: return False
+    if len(called_nums) == 1: return True
+
+    previous_num = called_nums[-2]
+    return (latest_num - previous_num == 1)
+
 def player_lost() -> bool:
     continue_playing = True
     
@@ -11,4 +23,31 @@ def player_lost() -> bool:
     return continue_playing
 
 def player_turn(called_numbers: list) -> tuple[bool, bool]:
-    return False, True
+
+    game_ended = False
+
+    print("HOW MANY NUMBERS WOULD YOU LIKE TO CALL?")
+    print("YOU MAY CALL UP TO 3 NUMBERS!")
+    added_amount = int(input("> "))
+
+    if added_amount > 3 or added_amount < 0: game_ended = True
+
+    print("EXCELLENT. PLEASE ENTER THE NUMBERS YOU WOULD LIKE TO CALL:")
+
+    if called_numbers and not game_ended: 
+        print(f"CURRENT NUMBERS: {called_numbers}")
+    else:
+        print("NO NUMBERS HAVE BEEN CALLED. PLEASE CALL CONSECUTIVE NUMBERS STARTING FROM 1!")
+
+    added_count = 0
+
+    while not game_ended and (added_count < added_amount): 
+        added_num = int(input("> "))
+
+        called_numbers.append(added_num)
+        game_ended = not verify_input(called_numbers)
+        added_count += 1
+
+    if (game_ended): return game_ended, player_lost()
+    return game_ended, True
+
