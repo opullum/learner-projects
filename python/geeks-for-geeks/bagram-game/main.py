b26a029afa4dfd2f34ead9c26645822dfb26e841
opullum <octavian.pullum@gmail.com>
2025-09-02 22:50:48 +0900

    Temporary implementation of game loop using while loops. Will need to fix later.

--- python/geeks-for-geeks/bagram-game/main.py
+++ python/geeks-for-geeks/bagram-game/main.py
@@ -42,24 +42,37 @@ Else:
 import sys
 
 from computer import computer_turn
-from player import player_lost
+from player import player_turn
 
 called_numbers = []
 recent_number = None
 game_ended = False
 playing = True
 
-print(f"WELCOME TO BAGRAM. WOULD YOU LIKE TO GO FIRST OR SECOND?")
-print("ENTER 'FIRST' TO GO FIRST AND 'SECOND' TO GO SECOND")
-response = input("> ").lower()
+while playing:
+    print(f"WELCOME TO BAGRAM. WOULD YOU LIKE TO GO FIRST OR SECOND?")
+    print("ENTER 'FIRST' TO GO FIRST AND 'SECOND' TO GO SECOND")
+    response = input("> ").lower()
 
-if not (response == "first" or response == "second"):
-    print("I'M AFRAID THAT IS NOT A VALID OPTION. GOODBYE.")
-    playing = False
+    if not (response == "first" or response == "second"):
+        print("I'M AFRAID THAT IS NOT A VALID OPTION. GOODBYE.")
+        playing = False
 
-if (response == "second"):
-    print("THEN YOU WILL GO SECOND. ALLOW US TO BEGIN.")
-    game_ended, playing = computer_turn(called_numbers)
+    if (response == "second"):
+        print("THEN YOU WILL GO SECOND. ALLOW US TO BEGIN.")
+
+        while (not game_ended):
+            if (not game_ended): game_ended, playing = computer_turn(called_numbers)
+            if (not game_ended): game_ended, playing = player_turn(called_numbers)
+    elif (response == "first"):
+        print("THEN YOU WILL GO FIRST. ALLOW US TO BEGIN.")
+
+        while (not game_ended):
+            if (not game_ended): game_ended, playing = player_turn(called_numbers)
+            if (not game_ended): game_ended, playing = computer_turn(called_numbers)
+            
+
+
 
     
 
