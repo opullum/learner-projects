c9aaafec3fceb494499862ef2edd322721da5b4b
opullum <octavian.pullum@gmail.com>
2025-09-04 00:58:36 +0900

    Added note regarding calling non-consecutive numbers.

--- python/geeks-for-geeks/bagram-game/main.py
+++ python/geeks-for-geeks/bagram-game/main.py
@@ -3,7 +3,7 @@
 # Simulating the game 'Bagram', a game where the player avoids being the one
 # to say the number 21. Players may choose to call up to 3 consecutive numbers.
 #
-# Calling non-consecutive numbers is an immediate loss. 
+# Calling non-consecutive numbers is an immediate loss.
 
 import sys
 
